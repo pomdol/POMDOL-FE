4f6749a0d65a5848b49ee1643444c3debd6c513b
HY KIM <chopinoff@gmail.com>
2024-05-21 11:58:40 +0900

    design : edit global style

--- pomdol/src/app/page.tsx
+++ pomdol/src/app/page.tsx
@@ -4,10 +4,17 @@ import { GlobalStyle } from '@/styles/globalStyle';
 import { darkTheme } from '@/styles/theme';
 import { ThemeProvider, styled } from 'styled-components';
 
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.color.bg1};
+`;
+
 export default function Home() {
   return (
     <ThemeProvider theme={darkTheme('desktop')}>
       <GlobalStyle />
+      <Main>TEST</Main>
     </ThemeProvider>
   );
 }
