7f20f339b5b58a5e739973eae5dbbbf9e0426edc
kimhhyeonjin <kimhhyeonjin@gmail.com>
2024-05-23 14:46:17 +0900

    feat : edit folder structure including routes

---
+++ pomdol/src/app/(afterLogin)/@modal/(.)setting/page.tsx
@@ -0,0 +1,5 @@
+import SettingModal from '@/app/_components/SettingModal';
+
+export default function Setting() {
+  return <SettingModal />;
+}
---
+++ pomdol/src/app/(afterLogin)/group/setting/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function GroupSetting() {
+  return <div></div>;
+}
